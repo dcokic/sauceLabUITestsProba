8aa2d055c76ff8cda8236d1c32560df5181d0800
dcokic <darijacokic@gmail.com>
2022-01-03 16:24:30 +0100

    dodala sam test

---
+++ src/tests/GakeTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class GakeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world lokalno");
+
+	}
+
+}
